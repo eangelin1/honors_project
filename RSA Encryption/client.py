c7edfde52044f8c07155c84b0fc1e6e39f26384a
Ethan Angelini <61705742+eangelin1@users.noreply.github.com>
2021-03-29 22:05:22 -0400

    client/server full funcitonaliy

--- RSA Encryption/client.py
+++ RSA Encryption/client.py
@@ -1,9 +1,44 @@
-import rsa
+#import rsa
 import socket
+import tqdm
+import os
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 # Each step will send this amount
+
+HOST = '192.168.0.39'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
+filename = "SEND/data.txt" # The file to send
+print(filename)
+filesize = os.path.getsize(filename) # The size of the file to send
 
+# Connecting to host with port
+s = socket.socket()
+print(f"[+] Connecting to {HOST}:{PORT}")
+s.connect((HOST, PORT))
+print("[+] Connected.")
+
+# Sending filename and size
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+# Sending file itself
+progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "rb") as f:
+    while True:
+        # read the bytes from the file
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            # then sending is complete
+            break
+        # we use sendall to assure transmission in busy networks
+        s.sendall(bytes_read)
+        # update progress bar
+        progress.update(len(bytes_read))
+
+# Close socket
+s.close()
+
+'''
 def server_listen():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
@@ -31,4 +66,4 @@ uMess = rsa.decrypt(message, bob_priv)
 dMess = uMess.decode('utf8')
 
 print(dMess)
-
+'''
--- RSA Encryption/server.py
+++ RSA Encryption/server.py
@@ -1,6 +1,67 @@
-import rsa
+#import rsa
 import socket
+import tqdm
+import os
 
+# Device IP Address
+SERVER_HOST = "192.168.0.39"
+SERVER_PORT = 65432
+
+# Receive 4096 bytes each step
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+
+# Create server socket
+# TCP socket
+s = socket.socket()
+
+# Bind socket to local address
+s.bind((SERVER_HOST, SERVER_PORT))
+
+# Enable server to accept connections
+# 5 here is the number of unaccepted connections
+# that the system will allow before refusing new connections
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+# Accept connection if there is any
+client_socket, address = s.accept()
+
+# If below code is executed, that means the sender is connected
+print(f"[+] {address} is connected")
+
+# Recieve file info
+# Recieve using client socket, not server socket
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+
+# Remove absolute path if there is one
+filename = "RECEIVE/"+os.path.basename(filename)
+# Convert to integer
+filesize = int(filesize)
+
+# Start receiving file from the socket and writing to the file stream
+progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+
+with open(filename, "wb") as f:
+    while True:
+        # read 1024 bytes from socket (receive)
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:
+            # Nothing is recieved
+            # File transmission done
+            break
+        # Write to file the bytes just received
+        f.write(bytes_read)
+        # Update progress bar
+        progress.update(len(bytes_read))
+
+# close client socket
+client_socket.close()
+# close server socket
+s.close()
+
+'''
 message = input("What do you wanna say to Bob: ")
 
 HOST = '127.0.0.1' # localhost
@@ -36,3 +97,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(crypto)
     data = s.recv(1024)
+'''
