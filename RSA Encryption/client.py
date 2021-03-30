25b07101b833fb6c4862d6ddba0827a1219eb6ac
Ethan Angelini <61705742+eangelin1@users.noreply.github.com>
2021-03-29 22:07:28 -0400

    deleted unecessary comments

--- RSA Encryption/client.py
+++ RSA Encryption/client.py
@@ -38,32 +38,3 @@ with open(filename, "rb") as f:
 # Close socket
 s.close()
 
-'''
-def server_listen():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                print("received data...")
-                if not data or data=='':
-                    print("NO DATA")
-                    break
-                else:
-                    print("sending data...")
-                    print(data)
-                    conn.sendall(data)
-                    return data
-
-bob_priv = rsa.PrivateKey.load_pkcs1('PEM')
-
-message = server_listen()
-uMess = rsa.decrypt(message, bob_priv)
-
-dMess = uMess.decode('utf8')
-
-print(dMess)
-'''
